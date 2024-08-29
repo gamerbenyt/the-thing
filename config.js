826843fc7c8c8ab5362528726298cc07d171c014
gamerbenyt <136914442+gamerbenyt@users.noreply.github.com>
2024-08-29 10:50:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     token: "YOUR_DISCORD_BOT_TOKEN",
-    channel: "XXXXXXXXXXXXXXXXXX",
-    messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
-    channel_id: "YOUTUBE_CHANNEL_ID",
+    channel: "1092203765345570937",
+    messageTemplate: "Hey <@&1191730867379380275>! **{author}** just posted a video! Go check it out! \n{url}",
+    channel_id: "UCze2gyIn3DneZCmJlZWnOUA",
     watchInterval: 30000
 }
